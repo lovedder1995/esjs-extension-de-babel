ab3f5467ad53d0458de8193065f9abe99eedad6a
Edder Alberto López Villastrigo <lovedder1995@outlook.com>
2025-08-02 22:14:55 -0600

    Importación de la extensión de babel

---
+++ src/generator.ts
@@ -0,0 +1,129 @@
+import type { NamedToken, Token, ValuedToken } from './token'
+import { TokenType } from './token'
+import { getDictionary } from './keywords'
+
+export function generate(tokens: Token[], reverse = false): string {
+  const dictionary: Map<string, string> = getDictionary(reverse)
+  let output = ''
+  let current = 0
+
+  /**
+   * Checks if we've consumed all the tokens
+   */
+  function isAtEnd() {
+    return current >= tokens.length
+  }
+
+  /**
+   * Returns true if the current token is of the given type
+   */
+  function check(type: TokenType) {
+    if (isAtEnd()) return false
+    return peek().type === type
+  }
+
+  /**
+   * Returns the current token without consuming it
+   */
+  function peek() {
+    return tokens[current]
+  }
+
+  /**
+   * Consumes and returns the current token
+   */
+  function advance() {
+    if (!isAtEnd()) current++
+    return previous()
+  }
+
+  /**
+   * Returns the previous token
+   */
+  function previous() {
+    return tokens[current - 1]
+  }
+
+  /**
+   * Given a list of types, check if the current token matches
+   * one of the types and if so, consumes it. Otherwise, returns
+   * false.
+   */
+  function match(...types: TokenType[]): boolean {
+    for (const type of types) {
+      if (check(type)) {
+        advance()
+        return true
+      }
+    }
+    return false
+  }
+
+  /**
+   * Consumes and returns the current token if it matches the provided type.
+   * Throws a syntax error if the token doesn't match the type.
+   */
+  function consume(type: TokenType) {
+    if (check(type)) return advance()
+    throw new SyntaxError(
+      `Unexpected token: Expected ${type}, found ${peek().type}`,
+    )
+  }
+
+  /**
+   * Repeatedly calls `consume` until `current` points to a token of the given
+   * type. Assumes `consume` updates the current token pointer.
+   */
+  function consumeUntil(type: TokenType, consume: () => void) {
+    while (!isAtEnd() && !match(type)) consume()
+
+    /**
+     * Throw a syntax error if we reached the end of input and didn't
+     * find the type we were looking for.
+     */
+    if (isAtEnd() && previous().type !== type)
+      throw new SyntaxError(`Unexpected end of file. Expected ${type}`)
+  }
+
+  function compileNamedToken(token: NamedToken) {
+    return dictionary.get(token.name) || token.name
+  }
+
+  function compileValuedToken(token: ValuedToken) {
+    return token.value
+  }
+
+  function compileStatement() {
+    let statement = ''
+    const token = peek()
+
+    switch (token.type) {
+      case TokenType.Keyword:
+      case TokenType.Identifier:
+        statement += compileNamedToken(token as NamedToken)
+        break
+      case TokenType.SpecialCharacter:
+      case TokenType.StringLiteral:
+      case TokenType.Whitespace:
+      case TokenType.Comment:
+      case TokenType.LeftParen:
+      case TokenType.RightParen:
+      case TokenType.LeftCurly:
+      case TokenType.RightCurly:
+      case TokenType.Dot:
+      case TokenType.Semicolon:
+        return compileValuedToken(token as ValuedToken)
+      default:
+        throw new SyntaxError(`Unexpected token: ${token.type}`)
+    }
+
+    return statement
+  }
+
+  while (!isAtEnd()) {
+    output += compileStatement()
+    advance()
+  }
+
+  return output
+}
---
+++ test/knownIssues.test.ts
@@ -0,0 +1,58 @@
+import { describe, expect, it } from 'vitest'
+import { compile } from '../src'
+
+describe('known issues', () => {
+  it('compiles variables named `escribir`', () => {
+    const esjs = `
+    const escribir = 'prueba'
+`
+
+    const js = `
+    const log = 'prueba'
+`
+
+    const compiled = compile(esjs)
+
+    expect(compiled).toEqual(js)
+
+    expect(compile(js, true)).toEqual(esjs)
+  })
+
+  it.skip('fixed: does not compile variables named `get`', () => {
+    // Deprecado: Ahora `mut` equivale a `let`, y `var` equivale a `var`.
+    const esjs = `
+    mut get = 'prueba'
+`
+
+    const js = `
+    let get = 'prueba'
+`
+
+    const compiled = compile(esjs)
+
+    expect(compiled).toEqual(js)
+
+    expect(compile(js, true)).toEqual(esjs)
+  })
+
+  it('does not compile var/global to var', () => {
+    const esjs = `
+    var x = 1
+    var y = 2
+`;
+
+    const js = `
+    var x = 1
+    var y = 2
+`
+
+    const compiled = compile(esjs)
+
+    expect(compiled).toEqual(js)
+
+    expect(compile(js, true)).toEqual(`
+    var x = 1
+    var y = 2
+`)
+  })
+})
